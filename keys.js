a22cafd93cfbcd41aee964dcfbaf3956705f3ddf
Lisa VanOrdenKaan <lmvanorden@gmail.com>
2016-04-15 20:47:51 -0400

    2nd time

--- keys.js
+++ keys.js
@@ -1,8 +1,15 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '<RSAGqjjv8SbfFlK6rzBO8XHU5>',
+  consumer_secret: '<VEvUS5m5KTb4b489YRah4A4kvemKCop5KQynG8CP8sLdrDCiO9>',
+  access_token_key: '<410457844-dUEoNCLLEaPAI36MWzly3ZzJrRLrIMEyLq2BoAnx>',
+  access_token_secret: '<FmyX58F6u2UCZkIzPHk23Qfanswg2Ub1e9PH9DjMyCbTr>',
+}
+
+exports.spotify = {
+  consumer_key: '<R>'
+  consumer_secret: '<VE>',
+  access_token_key: '<4>',
+  access_token_secret: '<L>',
 }
---
+++ liri.js
@@ -0,0 +1,14 @@
+var twitter = require('twitter')   // -- looks for npm psvkshr
+var keys = require('./keys.js')   // looks for a file 
+
+// my-tweets
+
+// spotify-this-song
+
+// movie-this
+
+// do-what-it-says
+
+// So if you succeed, running the following commands in your terminal will do the following things
+
+// node liri.js my-tweets
