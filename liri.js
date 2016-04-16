331fd3a70b1b243717711412ac35d7999c6a8b17
Lisa VanOrdenKaan <lmvanorden@gmail.com>
2016-04-15 21:30:19 -0400

    third

--- liri.js
+++ liri.js
@@ -1,6 +1,24 @@
 var twitter = require('twitter')   // -- looks for npm psvkshr
 var keys = require('./keys.js')   // looks for a file 
 
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+ 
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+
+
 // my-tweets
 
 // spotify-this-song
